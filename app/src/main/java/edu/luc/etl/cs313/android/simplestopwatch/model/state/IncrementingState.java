83c25e7c31f61a7e93faf62922164ce43fba1b9f
Katarina Krivokapic <katarina03kk@gmail.com>
2024-12-01 19:46:21 -0600

    added class file for IncrementingState

---
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/IncrementingState.java
@@ -0,0 +1,45 @@
+package edu.luc.etl.cs313.android.simplestopwatch.model.state;
+
+import edu.luc.etl.cs313.android.simplestopwatch.R;
+
+class IncrementingState implements StopwatchState {
+
+    private final StopwatchSMStateView sm;
+
+    public IncrementingState(final StopwatchSMStateView sm) {
+        this.sm = sm;
+    }
+
+    @Override
+    public void onStartStop() {
+        sm.actionStop();
+        sm.toStoppedState();
+    }
+
+    @Override
+    public void onLapReset() {
+        sm.toStoppedState();
+        sm.actionReset();
+    }
+
+    @Override
+    public void onIncrement() {
+        sm.actionIncCount();
+        sm.toIncrementingState();
+    }
+
+    @Override
+    public void onTick() {
+        throw new UnsupportedOperationException("onTick");
+    }
+
+    @Override
+    public void updateView() {
+        sm.updateUIRuntime();
+    }
+
+    @Override
+    public int getId() {
+        return R.string.INCREMENTING;
+    }
+}
